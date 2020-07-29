39cb9c468013c108e9e783ff99cfcb382e1ca176
MatriMariem <meriemmatri1994@gmail.com>
2020-07-29 08:27:25 +0100

    take the rest of line as it is

--- markdown2html.py
+++ markdown2html.py
@@ -22,10 +22,11 @@ if __name__ == "__main__":
         for line in data:
             line = line.split('\n')[0]
             words = line.split(' ')
-            if words[0][0] == '#':
-                open_tag = '<h' + str(len(words[0])) + '>'
-                close_tag = '</h' + str(len(words[0])) + '>'
-                html_line = open_tag + ' '.join(words[1:]) + close_tag + '\n'
+            size = len(words[0])
+            if words[0] == '#' * size :
+                open_tag = '<h' + str(size) + '>'
+                close_tag = '</h' + str(len(size) + '>'
+                html_line = open_tag + line[size + 1]) + close_tag + '\n'
                 f2.write(html_line)
             else:
                 html.append(line)
